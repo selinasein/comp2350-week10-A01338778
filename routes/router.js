59ce1ce07b309f68e4074d4f598a450757354b2f
selinasein <neulmisscj@gmail.com>
2023-03-23 00:34:54 -0700

    edited

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 // const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
 const userModel = include("models/web_user");
+const crypto = require("crypto");
+const { v4: uuid } = require("uuid");
+const passwordPepper = "SeCretPeppa4MySal+";
 
 router.get("/", async (req, res) => {
   console.log("page hit");
@@ -58,6 +61,27 @@ router.get("/", async (req, res) => {
 //   }
 // });
 
+router.get("/deleteUser", async (req, res) => {
+  try {
+    console.log("delete user");
+    let userId = req.query.id;
+    if (userId) {
+      console.log("userId: " + userId);
+      let deleteUser = await userModel.findByPk(userId);
+      console.log("deleteUser: ");
+      console.log(deleteUser);
+      if (deleteUser !== null) {
+        await deleteUser.destroy();
+      }
+    }
+    res.redirect("/");
+  } catch (ex) {
+    res.render("error", { message: "Error connecting to MySQL" });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
+});
+
 // router.get("/deleteUser", async (req, res) => {
 //   console.log("delete user");
 //   console.log(req.query);
@@ -73,5 +97,28 @@ router.get("/", async (req, res) => {
 //     }
 //   }
 // });
+
+router.post("/addUser", async (req, res) => {
+  try {
+    console.log("form submit");
+    const password_salt = crypto.createHash("sha512");
+    password_salt.update(uuid());
+    const password_hash = crypto.createHash("sha512");
+    password_hash.update(req.body.password + passwordPepper + password_salt);
+    let newUser = userModel.build({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password_salt: password_salt.digest("hex"),
+      password_hash: password_hash.digest("hex"),
+    });
+    await newUser.save();
+    res.redirect("/");
+  } catch (ex) {
+    res.render("error", { message: "Error connecting to MySQL" });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
+});
 
 module.exports = router;
