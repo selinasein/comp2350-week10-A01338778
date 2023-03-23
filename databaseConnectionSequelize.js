98f13e66bfb8869d3069e1ae61395255f58f9992
selinasein <neulmisscj@gmail.com>
2023-03-23 00:10:27 -0700

    edited

---
+++ databaseConnectionSequelize.js
@@ -0,0 +1,13 @@
+const is_qoddi = process.env.IS_QODDI || false;
+
+const dbConfigQoddi =
+  "mysql://freedb_2350_main_sel:Qk28*ry9NTw*w8z@sql.freedb.tech:3306/ freedb_comp2350-A01338778";
+const dbConfigLocal = "mysql://root:MySQLtpdls7^*@localhost/database";
+
+if (is_qoddi) {
+  var databaseConnectionString = dbConfigQoddi;
+} else {
+  var databaseConnectionString = dbConfigLocal;
+}
+
+module.exports = databaseConnectionString;
--- routes/router.js
+++ routes/router.js
@@ -1,22 +1,77 @@
-const router = require('express').Router();
-const database = include('databaseConnection');
-const dbModel = include('databaseAccessLayer');
+const router = require("express").Router();
+// const database = include('databaseConnection');
+// const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
+const userModel = include("models/web_user");
 
-router.get('/', async (req, res) => {
-	console.log("page hit");
-	
-	try {
-		const result = await dbModel.getAllUsers();
-		res.render('index', {allUsers: result});
+router.get("/", async (req, res) => {
+  console.log("page hit");
 
-		//Output the results of the query to the Heroku Logs
-		console.log(result);
-	}
-	catch (err) {
-		res.render('error', {message: 'Error reading from MySQL'});
-		console.log("Error reading from mysql");
-	}
+  try {
+    const users = await userModel.findAll({
+      attributes: ["web_user_id", "first_name", "last_name", "email"],
+    }); //{where: {web_user_id: 1}}
+    if (users === null) {
+      res.render("error", { message: "Error connecting to MySQL" });
+      console.log("Error connecting to userModel");
+    } else {
+      console.log(users);
+      res.render("index", { allUsers: users });
+    }
+  } catch (ex) {
+    res.render("error", { message: "Error connecting to MySQL" });
+    console.log("Error connecting to MySQL");
+    console.log(ex);
+  }
 });
+
+// router.get("/", async (req, res) => {
+//   console.log("page hit");
+
+//   try {
+//     const result = await dbModel.getAllUsers();
+//     res.render("index", { allUsers: result });
+
+//     //Output the results of the query to the Heroku Logs
+//     console.log(result);
+//   } catch (err) {
+//     res.render("error", { message: "Error reading from MySQL" });
+//     console.log("Error reading from mysql");
+//   }
+// });
+
+// router.post("/addUser", async (req, res) => {
+//   console.log("form submit");
+//   console.log(req.body);
+//   try {
+//     const success = await dbModel.addUser(req.body);
+//     if (success) {
+//       res.redirect("/");
+//     } else {
+//       res.render("error", { message: "Error writing to MySQL" });
+//       console.log("Error writing to MySQL");
+//     }
+//   } catch (err) {
+//     res.render("error", { message: "Error writing to MySQL" });
+//     console.log("Error writing to MySQL");
+//     console.log(err);
+//   }
+// });
+
+// router.get("/deleteUser", async (req, res) => {
+//   console.log("delete user");
+//   console.log(req.query);
+//   let userId = req.query.id;
+//   if (userId) {
+//     const success = await dbModel.deleteUser(userId);
+//     if (success) {
+//       res.redirect("/");
+//     } else {
+//       res.render("error", { message: "Error writing to MySQL" });
+//       console.log("Error writing to mysql");
+//       console.log(err);
+//     }
+//   }
+// });
 
 module.exports = router;
