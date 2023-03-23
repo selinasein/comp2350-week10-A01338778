1c5262dbd7e24b32a08a4325f514b2f2ca8b9ee1
selinasein <neulmisscj@gmail.com>
2023-03-23 00:20:56 -0700

    edited

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,8 +1,8 @@
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi =
-  "mysql://freedb_2350_main_sel:Qk28*ry9NTw*w8z@sql.freedb.tech:3306/ freedb_comp2350-A01338778";
-const dbConfigLocal = "mysql://root:MySQLtpdls7^*@localhost/database";
+  "mysql://freedb_2350_main_sel:Qk28*ry9NTw*w8z@sql.freedb.tech:3306/freedb_comp2350-A01338778";
+const dbConfigLocal = "mysql://root:MySQLtpdls7^*@localhost/week10_lab";
 
 if (is_qoddi) {
   var databaseConnectionString = dbConfigQoddi;
